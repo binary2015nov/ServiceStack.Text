b822c667883966ced136c78c37213fce489efd22
申行 <binary2015nov@outlook.com>
2017-10-11 11:02:06 +0800

    update AssemblyInfo.cs

--- build/copy.bat
+++ build/copy.bat
@@ -12,6 +12,6 @@ COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\Servic
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard2.0\ServiceStack.Text.* ..\..\ServiceStack.Aws\lib\netstandard2.0
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\ServiceStack.Admin\lib\net45
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard2.0\ServiceStack.Text.* ..\..\ServiceStack.Admin\lib\netstandard2.0
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\Stripe\lib\net45
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard2.0\ServiceStack.Text.* ..\..\Stripe\lib\netstandard2.0
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\ServiceStack.Stripe\lib\net45
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard2.0\ServiceStack.Text.* ..\..\ServiceStack.Stripe\lib\netstandard2.0
 
